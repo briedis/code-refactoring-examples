b9ecf308c281c3f3ce202af7eabe68a8aea11741
martins.briedis <mbriedis@gmail.com>
2017-07-05 16:05:24 +0300

    Hardcoded values update

--- hardcoded-values.php
+++ hardcoded-values.php
@@ -2,21 +2,15 @@
 
 class OrderService
 {
-    public function process()
-    {
-        $orders = $this->getOrders();
-
-        foreach ($orders as $order) {
-            if ($order->status !== 'fulfilled') {
-                $order->status = 'fulfilled';
-                $order->save();
-            }
+    public function process($order) {
+        if ($order->status !== 'fulfilled') {
+            $order->status = 'fulfilled';
+            $order->save();
         }
     }
 
-    public function getFulfilledOrder()
-    {
-        return $this->repository->getByStatus('fulfilled');
+    public function getFulfilledOrder() {
+        return $this->getByStatus('fulfilled');
     }
 }
 
@@ -24,20 +18,14 @@ class OrderService
 {
     const FULFILLED = 'fulfilled';
 
-    public function process()
-    {
-        $orders = $this->getOrders();
-
-        foreach ($orders as $order) {
-            if ($order->status !== self::FULFILLED) {
-                $order->status = self::FULFILLED;
-                $order->save();
-            }
+    public function process($order) {
+        if ($order->status !== self::FULFILLED) {
+            $order->status = self::FULFILLED;
+            $order->save();
         }
     }
 
-    public function getFulfilledOrder()
-    {
-        return $this->repository->getByStatus(self::FULFILLED);
+    public function getFulfilledOrder() {
+        return $this->getByStatus(self::FULFILLED);
     }
 }
